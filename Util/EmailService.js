644f96d1334a5d71f4c3a7444e65839827dfed2a
rotneng <129745212+rotneng@users.noreply.github.com>
2026-01-08 16:46:47 +0100

    61st

--- Util/EmailService.js
+++ Util/EmailService.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
