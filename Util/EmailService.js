cc20793a58c8dff878bbea6805ef784b06855442
rotneng <129745212+rotneng@users.noreply.github.com>
2026-01-08 07:56:28 +0100

    57th

--- Util/EmailService.js
+++ Util/EmailService.js
@@ -4,11 +4,14 @@ require("dotenv").config();
 const createTransporter = () => {
   return nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,    
-    secure: true,    
+    port: 587,    
+    secure: false,    
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
     },
   });
 };
